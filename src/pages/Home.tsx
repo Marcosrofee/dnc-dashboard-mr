77116bc4db328c01ed872d8825b7b13a6cc170c8
Marcos Rogerio <seuemail@example.com>
2025-01-11 16:54:41 -0300

    Feat/card-component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
-import { Header } from '@/components'
+import { CardComponet, Header } from '@/components'
+import { Container } from '@mui/material'
 
 function Home() {
   return (
     <>
       <Header />
+      <Container maxWidth="lg">
+        <CardComponet>CARD</CardComponet>
+      </Container>
       <h1>Home</h1>
     </>
   )
